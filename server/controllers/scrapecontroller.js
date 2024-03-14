e9e098be4ff11956c0adc6fe1b0f21e10e219f3d
Ishpreet01 <122776900+Ishpreet01@users.noreply.github.com>
2024-03-14 14:25:32 +0530

    Deleted two files and added the keyword functionality

--- server/controllers/scrapecontroller.js
+++ server/controllers/scrapecontroller.js
@@ -1,26 +1,3 @@
-// const { PythonShell } = require('python-shell');
-
-// const scrapeHandler = async (req,res) =>{
-//     const options = {
-//         scriptPath: '../server/WebScraping', // Path to the directory containing your Python script
-//         timeout: 8000000, // Timeout in milliseconds
-//         // args: ['arg1', 'arg2'], // Optional arguments to pass to the Python script
-//     };
-
-//      try {
-//         const results = await new Promise((resolve, reject) => {
-//             PythonShell.run('scraper.py', options, (err, results) => {
-//                 if (err) reject(err);
-//                 else resolve(results);
-//             });
-//         });
-//         console.log('Python script output:', results);
-//         res.status(200).json({ message: "Scrape successful", data: results });
-//     } catch (err) {
-//         res.status(500).json({ message: "Error during scraping", error: err.toString() });
-//     }
-// };
-
 const scrapeHandler = async (req,res) =>{
     try {
         // Forward the request to the Flask server
